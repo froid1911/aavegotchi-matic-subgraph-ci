c5b3adff6402280ef05e83dc687b6ea73ae92dad
Frank <frank.pfeift@protonmail.com>
2022-01-30 13:21:50 +0100

    changed imports

--- e2e/helper/checkSubgraph.js
+++ e2e/helper/checkSubgraph.js
@@ -2,13 +2,13 @@ const apollo = require("apollo-fetch");
 const ethers = require("ethers");
 const config = require("./config");
 
-console.log(config.rpcUrl);
-const provider = new ethers.providers.JsonRpcProvider(config.rpcUrl);
-const graph = apollo.createApolloFetch({ uri: config.endpoint });
-
-const query = `{_meta {block { hash number } deployment hasIndexingErrors}}`;
-
 async function checkSubgraph() {
+  console.log(config.rpcUrl);
+  const provider = new ethers.providers.JsonRpcProvider(config.rpcUrl);
+  const graph = apollo.createApolloFetch({ uri: config.endpoint });
+
+  const query = `{_meta {block { hash number } deployment hasIndexingErrors}}`;
+
   const { number } = await provider.getBlock("latest");
   const { data } = await graph({ query });
 
