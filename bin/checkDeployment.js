d64c1974c4691af52090b8feb867791345beec2a
Frank <frank.pfeift@protonmail.com>
2022-01-30 13:28:55 +0100

    added setTimeout

--- bin/checkDeployment.js
+++ bin/checkDeployment.js
@@ -1,11 +1,14 @@
+const config = require("../e2e/helper/config");
 const checkSubgraph = require("../e2e/helper/checkSubgraph");
 
-checkSubgraph().then((result) => {
-  if (!result) {
-    console.log("Subgraph not ready");
-    return process.exit(1);
-  }
+setTimeout(() => {
+  checkSubgraph().then((result) => {
+    if (!result) {
+      console.log("Subgraph not ready");
+      return process.exit(1);
+    }
 
-  console.log("Subgraph ready");
-  return process.exit(0);
-});
+    console.log("Subgraph ready");
+    return process.exit(0);
+  });
+}, 1000);
